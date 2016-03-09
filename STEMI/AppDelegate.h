443ce3a65c9cad29e898443a2678a221e3db3d53
Jasmin Abou Aldan <jasmin.aboualdan@gmail.com>
2016-03-09 19:56:29 +0100

    Initial commit

---
+++ STEMI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STEMI
+//
+//  Created by Jasmin Abou Aldan on 09/03/16.
+//  Copyright © 2016 Jasmin Abou Aldan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ STEMI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STEMI
+//
+//  Created by Jasmin Abou Aldan on 09/03/16.
+//  Copyright © 2016 Jasmin Abou Aldan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
