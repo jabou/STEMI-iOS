85aecfed60e19c7c84cea8f3e6f2c31fe0174345
Jasmin Abou Aldan <jasmin.aboualdan@gmail.com>
2016-08-14 13:51:50 +0200

    Fixed joystick bug when settings is closed and Menu animation speeded up

--- STEMI/JoystickViewController.swift
+++ STEMI/JoystickViewController.swift
@@ -19,14 +19,14 @@ class JoystickViewController: UIViewController, LeftJoystickViewDelegate, RightJ
     @IBOutlet weak var rightJoystickView: UIView!
     @IBOutlet weak var menuScreenView: UIButton!
     @IBOutlet weak var menuView: UIView!
-    
+
     //MARK: - variables
-    let selectedPictures = ["movement_sel","rotation_sel","orientation_sel","height_sel","settings_sel"];
-    let unselectedPictures = ["movement_non","rotation_non","orientation_non","height_non","settings_non"];
+    let selectedPictures = ["movement_sel", "rotation_sel", "orientation_sel", "height_sel", "settings_sel"]
+    let unselectedPictures = ["movement_non", "rotation_non", "orientation_non", "height_non", "settings_non"]
     var accelerometer: CMMotionManager!
     var accelerometerX: UInt8!
     var accelerometerY: UInt8!
-    
+
     //MARK: - objects
     var stemi: Hexapod!
     var menu: Menu!
@@ -34,119 +34,115 @@ class JoystickViewController: UIViewController, LeftJoystickViewDelegate, RightJ
     var rightJoystick: RightJoystickView!
     var toastNotification: ToastNotification!
 
-    
+
     //MARK: - Methods
-    override func viewDidAppear(animated: Bool) {
-        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
         //Put left joystick on main view
         leftJoystick = LeftJoystickView(frame: self.leftJoystickView.bounds)
         leftJoystick.leftDelegate = self
         self.leftJoystickView.addSubview(leftJoystick)
-        
+
         //Put right joystick on main view
         rightJoystick = RightJoystickView(frame: self.rightJoystickView.bounds)
         rightJoystick.rightDelegate = self
         self.rightJoystickView.addSubview(rightJoystick)
-        
+
         //Put menu on main view
         menu = Menu(frame: self.menuView.bounds)
         menu.delegate = self
         menuDidBecomeInactive()
         self.menuView.addSubview(menu)
-        
-        //Setup stemi, and start connection
-        stemi = Hexapod()
-        startConnection()
-        
-        //Declare accelerometer and start takeing values from accelerometer (on main thread)
-        accelerometer = CMMotionManager()
-        accelerometer.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue()) {
-            (data:CMAccelerometerData?, error: NSError?) in
-            
-            
-            
-            if let acceleration = data?.acceleration {
-                if acceleration.x < -0.4 {
-                    self.accelerometerX = 40
-                }
-                else if acceleration.x > 0.4{
-                    self.accelerometerX = 216
-                }
-                else {
-                    if acceleration.x > 0 {
-                        self.accelerometerX = UInt8(min(256 - acceleration.x * 100,255))
-                    } else {
-                        self.accelerometerX = UInt8(-1 * acceleration.x * 100)
-                    }
-                }
-                
-                if acceleration.y < -0.4 {
-                    self.accelerometerY = 40
-                }
-                else if acceleration.y > 0.4{
-                    self.accelerometerY = 216
-                }
-                else {
-                    if acceleration.y > 0 {
-                        self.accelerometerY = UInt8(min(256 - acceleration.y * 100,255))
-                    } else {
-                        self.accelerometerY = UInt8(-1 * acceleration.y * 100)
-                    }
-                }
-            }
-            
-            self.stemi.setAccX(self.accelerometerX)
-            self.stemi.setAccY(self.accelerometerY)
-            
-        }
 
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
         //Hide status bar and rotate
         UIApplication.sharedApplication().statusBarHidden = true
         //UIDevice.currentDevice().setValue(UIInterfaceOrientation.LandscapeRight.rawValue, forKey: "orientation")
-        
+
         //Setup standby button on screen, and put value on "selected" (active)
         standbyButton.setImage(UIImage(named: "standby_off"), forState: .Normal)
         standbyButton.setImage(UIImage(named: "standby_on"), forState: .Selected)
         standbyButton.setImage(UIImage(named: "standby_on"), forState: .Highlighted)
         standbyButton.selected = true
-        
+
         //Add notification observers for start and stop connection with stemi
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(JoystickViewController.stopConnection), name: "StopConnection", object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(JoystickViewController.startConnection), name: "StartConnection", object: nil)
         
     }
-    
+
+    override func viewDidAppear(animated: Bool) {
+
+        //Setup stemi, and start connection
+        stemi = Hexapod()
+        startConnection()
+
+        //Declare accelerometer and start takeing values from accelerometer (on main thread)
+        accelerometer = CMMotionManager()
+        accelerometer.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue()) {
+            (data: CMAccelerometerData?, error: NSError?) in
+
+
+
+            if let acceleration = data?.acceleration {
+                if acceleration.x < -0.4 {
+                    self.accelerometerX = 40
+                } else if acceleration.x > 0.4 {
+                    self.accelerometerX = 216
+                } else {
+                    if acceleration.x > 0 {
+                        self.accelerometerX = UInt8(min(256 - acceleration.x * 100, 255))
+                    } else {
+                        self.accelerometerX = UInt8(-1 * acceleration.x * 100)
+                    }
+                }
+
+                if acceleration.y < -0.4 {
+                    self.accelerometerY = 40
+                } else if acceleration.y > 0.4 {
+                    self.accelerometerY = 216
+                } else {
+                    if acceleration.y > 0 {
+                        self.accelerometerY = UInt8(min(256 - acceleration.y * 100, 255))
+                    } else {
+                        self.accelerometerY = UInt8(-1 * acceleration.y * 100)
+                    }
+                }
+            }
+
+            self.stemi.setAccX(self.accelerometerX)
+            self.stemi.setAccY(self.accelerometerY)
+
+        }
+
+    }
+
     override func shouldAutorotate() -> Bool {
         return false
     }
-    
+
     override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
         return UIInterfaceOrientationMask.LandscapeRight
     }
-    
+
     //MARK: Menu methods
     func menuDidBecomeActive() {
         self.menuScreenView.hidden = false
         self.view.bringSubviewToFront(self.menuView)
     }
-    
+
     func menuDidBecomeInactive() {
         self.menuScreenView.hidden = true
         self.view.insertSubview(self.menuView, aboveSubview: self.backgroundView)
     }
-    
+
     func menuDidChangePlayMode(mode: String) {
         switch mode {
         case "movement":
             toastNotification = ToastNotification(onView: self.view, isHint: false, headline: "movement", text: nil, height: nil)
             toastNotification.showNotificationWithAutohide()
             stemi.setMovementMode()
-            
+
         case "rotation":
             toastNotification = ToastNotification(onView: self.view, isHint: false, headline: "rotation", text: nil, height: nil)
             toastNotification.showNotificationWithAutohide()
@@ -158,12 +154,12 @@ class JoystickViewController: UIViewController, LeftJoystickViewDelegate, RightJ
         default:
             print("Mode did not changed!")
         }
-        
+
     }
-    
+
     func menuButtonLongPressOnIndex(index: Int, withState state: UIGestureRecognizerState) {
-        
-        if state == .Began{
+
+        if state == .Began {
             switch index {
             case 0:
                 toastNotification = ToastNotification(onView: self.view, isHint: true, headline: "movement", text: "allows linear movements (left, right, back, forward). Tap to enable.", height: 110)
@@ -186,28 +182,42 @@ class JoystickViewController: UIViewController, LeftJoystickViewDelegate, RightJ
             default:
                 print("Long press error")
             }
-        }
-        else if state == .Ended{
+        } else if state == .Ended {
             toastNotification.hideNotification()
         }
     }
-    
+
+    func menuButtonDidSelectOnIndex(index: Int) {
+        switch index {
+        case 3:
+            print("Index: \(index)")
+        case 4:
+            print("Index: \(index)")
+        case 5:
+            print("Index: \(index)")
+        case 6:
+            self.performSegueWithIdentifier("settings", sender: self)
+        default:
+            break
+        }
+    }
+
     //MARK: Joystick methods
     func leftJoystickDidMove(powerValue: UInt8, angleValue: UInt8) {
         stemi.setJoystickParams(powerValue, angle: angleValue)
     }
-    
+
     func rightJoystickDidMove(rotationValue: UInt8) {
         stemi.setJoystickParams(rotationValue)
     }
-    
-    
+
+
     //MARK: Connection methods
-    func startConnection(){
+    func startConnection() {
         stemi.connect()
     }
-    
-    func stopConnection(){
+
+    func stopConnection() {
         stemi.disconnect()
     }
 
@@ -215,14 +225,14 @@ class JoystickViewController: UIViewController, LeftJoystickViewDelegate, RightJ
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
+
+
     //MARK: - Button press handler
     //MARK: Menu mask (when menu is open, mask appears. On click, mask close menu and disapper)
     @IBAction func menuScreenViewPressed(sender: AnyObject) {
         menu.closeMenu()
     }
-    
+
     //MARK: Standby button
     @IBAction func standbyButtonPressed(sender: AnyObject) {
         if standbyButton.selected {
--- STEMI/Menu.swift
+++ STEMI/Menu.swift
@@ -14,109 +14,110 @@ protocol MenuViewDelegate {
     func menuDidBecomeInactive()
     func menuDidChangePlayMode(mode: String)
     func menuButtonLongPressOnIndex(index: Int, withState state: UIGestureRecognizerState)
+    func menuButtonDidSelectOnIndex(index: Int)
 }
 
 class Menu: UIView {
-    
+
     @IBOutlet var buttonCollection: [UIButton]!
     @IBOutlet var longPressCollection: [UILongPressGestureRecognizer]!
     @IBOutlet weak var view: UIView!
     @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var menuActiveIndicator: UIImageView!
     @IBOutlet weak var menuButton: UIButton!
-    
+
     var delegate: MenuViewDelegate?
     var width: CGFloat!
     var height: CGFloat!
-    
+
     var soundIn: AVPlayer!
     var soundOut: AVPlayer!
-        
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+
         NSBundle.mainBundle().loadNibNamed("Menu", owner: self, options: nil)
         width = frame.size.width
         height = frame.size.height
         self.view.frame = CGRectMake(0, 0, width, height)
         self.addSubview(self.view)
-        
+
         menuButton.selected = false
         backgroundImage.frame = CGRectMake(self.width/2, self.height, 0, 0)
         backgroundImage.hidden = true
         menuActiveIndicator.alpha = 0
         menuActiveIndicator.hidden = true
-        
-        
+
+
         self.buttonCollection[0].selected = true
-        for i in 0 ..< buttonCollection.count{
+        for i in 0 ..< buttonCollection.count {
             buttonCollection[i].alpha = 0
             buttonCollection[i].hidden = true
         }
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
-    func openMenu(){
+
+    func openMenu() {
 
         menuButton.selected = true
         menuButton.enabled = false
         self.menuActiveIndicator.hidden = false
         self.backgroundImage.hidden = false
         delegate?.menuDidBecomeActive()
-        
+
         let path = NSBundle.mainBundle().pathForResource("puk", ofType: "wav")
         let url = NSURL(fileURLWithPath: path!)
         soundIn = AVPlayer(URL: url)
         soundIn.play()
-        
+
         UIView.animateWithDuration(0.15, animations: {
             self.menuActiveIndicator.alpha = 1
             self.menuActiveIndicator.frame = CGRectMake(self.width/2 - self.menuActiveIndicator.bounds.size.width/2, self.height - 78, 75, 60)
 
         }) {(Bool) in
-            UIView.animateWithDuration(0.15, animations: { 
+            UIView.animateWithDuration(0.15, animations: {
                 self.menuActiveIndicator.frame = CGRectMake(self.width/2 - self.menuActiveIndicator.bounds.size.width/2, self.height - 68, 75, 60)
                 }, completion: nil)
         }
-        
-        UIView.animateWithDuration(0.3, animations: {
+
+        UIView.animateWithDuration(0.25, animations: {
             self.backgroundImage.alpha = 1
             self.backgroundImage.frame = CGRectMake(0, 0, self.width, self.height)
         }) {(Bool) in
             self.menuButton.enabled = true
-            
+
             var delay = 0.0
-            
+
             for i in 0 ..< self.buttonCollection.count {
-                
+
                 self.buttonCollection[i].hidden = false
-                
-                UIView.animateWithDuration(0.5, delay: delay, options: .CurveEaseInOut, animations: {
+
+                UIView.animateWithDuration(0.3, delay: delay, options: .CurveEaseInOut, animations: {
                     self.buttonCollection[i].alpha = 1
                     }, completion: nil)
-                
-                delay += 0.05
+
+                delay += 0.03
             }
-            
+
         }
     }
-    
-    func closeMenu(){
-        
+
+    func closeMenu() {
+
         menuButton.selected = false
         menuButton.enabled = false
         delegate?.menuDidBecomeInactive()
         let path = NSBundle.mainBundle().pathForResource("tiu", ofType: "wav")
         let url = NSURL(fileURLWithPath: path!)
         soundOut = AVPlayer(URL: url)
-        
-        UIView.animateWithDuration(0.01, animations: { 
+
+        UIView.animateWithDuration(0.01, animations: {
             self.menuActiveIndicator.frame = CGRectMake(self.width/2 - self.menuActiveIndicator.bounds.size.width/2 - 4, self.height - 68, 75, 60)
             }) { (Bool) in
-                UIView.animateWithDuration(0.01, animations: { 
+                UIView.animateWithDuration(0.01, animations: {
                     self.menuActiveIndicator.frame = CGRectMake(self.width/2 - self.menuActiveIndicator.bounds.size.width/2 + 4, self.height - 68, 75, 60)
 
                     }, completion: { (Bool) in
@@ -131,7 +132,7 @@ class Menu: UIView {
                                         UIView.animateWithDuration(0.15, animations: {
                                             self.menuActiveIndicator.alpha = 1
                                             self.menuActiveIndicator.frame = CGRectMake(self.width/2 - self.menuActiveIndicator.bounds.size.width/2, self.height - 88, 75, 60)
-                                            
+
                                         }) {(Bool) in
                                             UIView.animateWithDuration(0.15, animations: {
                                                 self.menuActiveIndicator.frame = CGRectMake(self.width/2 - self.menuActiveIndicator.bounds.size.width/2, self.height - 60, 75, 60)
@@ -144,10 +145,10 @@ class Menu: UIView {
                         })
                 })
         }
-        
+
         UIView.animateWithDuration(0.5, animations: {
             self.soundOut.play()
-            for i in 0 ..< self.buttonCollection.count{
+            for i in 0 ..< self.buttonCollection.count {
                 self.buttonCollection[i].alpha = 0
             }
             self.backgroundImage.alpha = 0
@@ -156,64 +157,62 @@ class Menu: UIView {
                 self.backgroundImage.frame = CGRectMake(self.width/2, self.height, 0, 0)
                 self.backgroundImage.hidden = true
                 self.menuButton.enabled = true
-                for i in 0 ..< self.buttonCollection.count{
+                for i in 0 ..< self.buttonCollection.count {
                     self.buttonCollection[i].hidden = true
                 }
         }
     }
-    
-    
+
+
     @IBAction func menuButtonTapped(sender: AnyObject) {
-        
+
         if menuButton.selected {
             closeMenu()
         } else {
             openMenu()
         }
-        
+
     }
-    
+
     @IBAction func buttonLongPressed(sender: UILongPressGestureRecognizer) {
         delegate?.menuButtonLongPressOnIndex(self.longPressCollection.indexOf(sender)!, withState: sender.state)
     }
-    
-     @IBAction func buttonPressed(sender: UIButton){
-     
+
+     @IBAction func buttonPressed(sender: UIButton) {
+
         let index = self.buttonCollection.indexOf(sender)!
         if index >= 0 && index <= 2 {
-            
+
             if index == 0 {
-                if !sender.selected{
+                if !sender.selected {
                     delegate?.menuDidChangePlayMode("movement")
                 }
                 closeMenu()
-            }
-            else if index == 1{
-                if !sender.selected{
+            } else if index == 1 {
+                if !sender.selected {
                     delegate?.menuDidChangePlayMode("rotation")
                 }
                 closeMenu()
-            }
-            else if index == 2{
-                if !sender.selected{
+            } else if index == 2 {
+                if !sender.selected {
                     delegate?.menuDidChangePlayMode("orientation")
                 }
                 closeMenu()
             }
-            
-            for i in 0 ..< 3{
-                
+
+            for i in 0 ..< 3 {
+
                 let objectUIButton: UIButton = self.buttonCollection[i]
                 objectUIButton.selected = false
-     
+
             }
-            
+
             sender.selected = true
-     
-        }
-        else {
+
+        } else {
+            delegate?.menuButtonDidSelectOnIndex(index)
             closeMenu()
         }
     }
- 
+
 }
